4a6d2a75ffcefaa05ee6cb28448594f02db67b1f
Florent Morel <flmorel.dev@gmail.com>
2017-09-23 15:29:46 +0200

    Fix configmap sql

--- resources/sql/05_configmap.sql
+++ resources/sql/05_configmap.sql
@@ -38,8 +38,6 @@ CREATE TABLE `configmap` (
 -- Contenu de la table `configmap`
 --
 -- Read it from prefs file
--- INSERT INTO `configmap` (`id_configmap`, `lat_configmap`, `lon_configmap`, `zoom_configmap`, `baselayer_configmap`) VALUES
-(1, 43.6032, 1.4321, 13, 0);
 
 --
 -- Index pour les tables exportées
